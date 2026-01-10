79ac861a6d35cb9733bb8f4ca785a82e35cd3174
Jorle <jorle112112@gmail.com>
2026-01-10 15:27:28 +0800

    for求和

---
+++ for求和.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int i=1;
+	int sum=0;
+	for(i=1;i<=100;i++){
+		sum=sum+i;
+		
+	}
+	printf("1到100的和为%d",sum);
+	return 0;
+} 
---
+++ if求和.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i;
+	int sum=0;
+	if(i=1,i<=100,i++){
+		sum=sum+i;
+		printf("1到100的和为%d",sum);
+	}
+	return 0;
+} 
